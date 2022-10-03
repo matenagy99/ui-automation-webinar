da4f25847b9b08ce6cb499c84c88b4c91210186b
Nagy Máté <Mate_Nagy2@epam.com>
2022-10-03 13:00:43 +0200

    actual state

--- cucumber/features/careerPage.feature
+++ cucumber/features/careerPage.feature
@@ -9,12 +9,18 @@ Feature: EPAM job searching
     Then Epam logo should be visible
     Then Job search form is visible
 
-    When Select <Country> and <City> in location filter box
+    When Select <Country> and <City> in the location filter box
     Then <City> should be displayed in the location filter box
 
+    When Select <Department> in the department filter box
+    Then <Department> should be displayed on the page
 
+    When Click on Find button
+    Then selected department should be displayed
+    Then There should be a job named <Job>
+    Then Should have <Country> and <City> as location
 
     Examples:
-    |Country  |City     |Department                            |
-    |Hugary   |Debrecen |Software, System, and Test Engineering|
+    |Country  |City     |Department                            |Job                                |
+    |Hungary  |Debrecen |Software, System, and Test Engineering|.NET Developer (Middle/Senior/Lead)|
   
--- cucumber/step_definitions/careerPage.js
+++ cucumber/step_definitions/careerPage.js
@@ -9,10 +9,10 @@ Given(/Epam careers page is loaded/, async () => {
 });
 
 Then(/Epam logo should be visible/ , async () => {
-    const logo = element(by.css("a.header__logo-container"));
+    const logo = element(by.css("img.header__logo"));
 
     const acceptCookies = element(by.css("#onetrust-accept-btn-handler"));
-    acceptCookies.click();
+    await acceptCookies.click();
 
     expect(logo.isDisplayed()).to.eventually.be.true;
 });
@@ -22,21 +22,58 @@ Then(/Job search form is visible/, async () => {
     expect(searchForm.isDisplayed()).to.eventually.be.true;
 });
 
-When(/Select (.*) and (.*) in location filter box/, async (country, city) => {
+When(/Select (.*) and (.*) in the location filter box/, async (country, city) => {
     const locationInput = element(by.css(".recruiting-search__location"));
-    locationInput.click();
-
+    await locationInput.click();
+    // await browser.wait(ec.visibilityOf(element(by.css("ul[id$=copy-location-results]"))), 5000);
     const filtering = element(by.css("input[role='combobox']"));
-    filtering.sendKeys(country.slice(0,4));
+    await filtering.sendKeys(country.slice(0,3));
 
     const countrySelect = element(by.css(`li[aria-label='${country}']`));
-    countrySelect.click();
+    await countrySelect.click();
+    // await browser.wait(ec.visibilityOf(element(by.css(`li[aria-label=${country}] ul li:last-child`))), 5000);
 
-    const citySelect = element(by.css(`li[aria-label='Hungary'] li[id*='${city}']`));
-    citySelect.click();
+    const citySelect = element(by.css(`li[aria-label='${country}'] ul li[id*='${city}']`));
+    await citySelect.click();
 });
 
 Then(/(.*) should be displayed in the location filter box/, async (city) => {
     const checkLocation = element(by.css(".recruiting-search__location .select2-selection__rendered"));
     expect(checkLocation.getText()).to.eventually.equal(city);
+});
+
+When(/Select (.*) in the department filter box/, async (department) => {
+    const departmentInput = element(by.css("#jobSearchFilterForm div[role='combobox']"));
+    expect(departmentInput.isDisplayed()).to.eventually.be.true;
+
+    await departmentInput.click();
+    await browser.wait(ec.visibilityOf(element(by.css(".multi-select-dropdown ul")), 2000));
+    const myDep = element(by.css(".multi-select-dropdown ul input[data-value='Software, System, and Test Engineering'] + span"));
+    await myDep.click();
+});
+
+Then(/(.*) should be displayed on the page/, async (department) => {
+    const selected = await element.all(by.css(".selected-items li"));
+    expect(selected).to.have.length(1);
+    expect(selected[0].getText()).to.eventually.equal((`${department}`).toUpperCase());
+});
+
+When(/Click on Find button/, async () => {
+    const findButton = element(by.css("#jobSearchFilterForm > button"));
+    await findButton.click();
+});
+
+Then(/selected department should be displayed/, async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then(/There should be a job named (.)/, async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+
+Then(/Should have Hungary and Debrecen as location/ , async () => {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
 });
