a65a4562ee9c06c18a35d910af13eac253b3a8e7
Nagy Máté <Mate_Nagy2@epam.com>
2022-10-03 10:20:55 +0200

    When-ben lévő action-ök nem futnak le

--- cucumber/features/careerPage.feature
+++ cucumber/features/careerPage.feature
@@ -2,3 +2,19 @@ Feature: EPAM job searching
   As a Job searcher
   I want to browser through EPAM Job offers by various criteria
   So I can find to best fitting offer for me
+
+  Scenario Outline: Searching for job
+
+    Given Epam careers page is loaded
+    Then Epam logo should be visible
+    Then Job search form is visible
+
+    When Select <Country> and <City> in location filter box
+    Then <City> should be displayed in the location filter box
+
+
+
+    Examples:
+    |Country  |City     |Department                            |
+    |Hugary   |Debrecen |Software, System, and Test Engineering|
+  
--- cucumber/step_definitions/careerPage.js
+++ cucumber/step_definitions/careerPage.js
@@ -3,3 +3,40 @@
 const { Given, When, Then, setDefaultTimeout } = require('cucumber');
 
 setDefaultTimeout(GLOBAL_TIMEOUT);
+
+Given("Epam careers page is loaded", async () => {
+    await browser.get("https://www.epam.com/careers");
+});
+
+Then("Epam logo should be visible" , async () => {
+    const logo = element(by.css("a.header__logo-container"));
+
+    const acceptCookies = element(by.css("#onetrust-accept-btn-handler"));
+    await acceptCookies.click();
+
+    expect(logo.isDisplayed()).to.eventually.be.true;
+});
+
+Then("Job search form is visible", async () => {
+    const searchForm = element(by.css("form#jobSearchFilterForm"));
+    expect(searchForm.isDisplayed()).to.eventually.be.true;
+});
+
+When(/Select (.*) and (.*) in location filter box/, async (country, city) => {
+    const locationInput = element(by.css(".recruiting-search__location"));
+    locationInput.click();
+
+    const filtering = element(by.css("input[role='combobox']"));
+    filtering.sendKeys(country.slice(0,4));
+
+    const countrySelect = element(by.css(`li[aria-label='${country}']`));
+    countrySelect.click();
+
+    const citySelect = element(by.css(`li[aria-label='Hungary'] li[id*='${city}']`));
+    citySelect.click();
+});
+
+Then(/(.*) should be displayed in the location filter box/, async (city) => {
+    const checkLocation = element(by.css(".recruiting-search__location .select2-selection__rendered"));
+    expect(checkLocation.getText()).to.eventually.equal(city);
+});
