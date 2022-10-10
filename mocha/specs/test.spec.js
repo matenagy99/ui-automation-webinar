3f1775e7a820522d4c46f3a00e1801cbbb626b20
Nagy Máté <Mate_Nagy2@epam.com>
2022-10-10 11:54:53 +0200

    Working

--- mocha/specs/test.spec.js
+++ mocha/specs/test.spec.js
@@ -5,29 +5,27 @@ const careerPage = new CareerPage();
 
 describe("Search for job", async () => {
 
-    beforeEach(async () => {
-        careerPage.load();
+    before(async () => {
+        await careerPage.load();
     });
 
     describe("Careers page", async () => {
         it("should be opnened", async () => {
             await careerPage.acceptCookiesButton.click();
-            return expect(careerPage.logo.isDisplayed()).to.eventually.be.true;
+            expect(careerPage.logo.isDisplayed()).to.eventually.be.true;
         });
     });
 
     describe("Search form", async () => {
         
         it("should be displayed", async () => {
-            return expect(careerPage.searchForm.isDisplayed()).to.eventually.be.true;
+            expect(careerPage.searchForm.isDisplayed()).to.eventually.be.true;
         });
 
         describe("Location filter box", async () => {
             it("should provide a way to filter to a specific location", async () => {
                 expect(careerPage.locationInput.isDisplayed()).to.eventually.be.true;
-
                 await careerPage.selectLocation("Hungary", "Debrecen");
-
                 expect(careerPage.checkLocation.getText()).to.eventually.equal("Debrecen");
             });
         });
@@ -35,80 +33,53 @@ describe("Search for job", async () => {
         describe("Department filter box", async () => {
             it("should provide a way to filter to a specific department", async () => {
                 expect(careerPage.departmentInput.isPresent()).to.eventually.be.true;
-
-                // await careerPage.selectDepartment("Software, System, and Test Engineering");
-
-                // expect(careerPage.checkSelectedDepartment).to.eventually.have.length(1);
-                // expect(careerPage.checkSelectedDepartment[0].getText()).to.eventually.equal(("Software, System, and Test Engineering").toUpperCase());
+                await careerPage.selectDepartment("Software, System, and Test Engineering");
+                expect(careerPage.checkSelectedDepartment.getText()).to.eventually.equal(("SOFTWARE, SYSTEM, AND TEST ENGINEERING"));
             });
         });
         
-    //     describe("Searching", async () => {
+        describe("Searching", async () => {
 
-    //         beforeEach(async () => {
-    //             const locationInput = element(by.css(".recruiting-search__location .select2-selection.select2-selection--single"));
-    //             locationInput.click();
-    //             const filtering = element(by.css("input[role='combobox']"));
-    //             filtering.sendKeys("Hu");
-    //             const country = element(by.css("li[aria-label='Hungary']"));
-    //             country.click();
-    //             const city = element(by.css("li[aria-label='Hungary'] li:nth-child(3)"));
-    //             city.click();
+            before(async () => {
+                await careerPage.clickFindButton();
+            });
 
-    //             const departmentInput = element(by.css("#jobSearchFilterForm div[role='combobox']"));
-    //             departmentInput.click();
-    //             await browser.wait(ec.visibilityOf(element(by.css(".multi-select-dropdown ul")), 3000));
-    //             const myDep = element(by.css(".multi-select-dropdown ul input[data-value='Software, System, and Test Engineering'] + span"));
-    //             myDep.click();
+            it("should find the proper job", async () => {
+                const searchedJob = careerPage.jobResult(careerPage.formatJobTitle(".NET Developer (Middle/Senior/Lead)"));
+                expect(searchedJob.getText()).to.eventually.equal(".NET Developer (Middle/Senior/Lead)");
+            });
 
-    //             const findButton = element(by.css("#jobSearchFilterForm > button"));
-    //             findButton.click();
+            it("should have job with proper department", async () => {
+                expect(careerPage.checkSelectedDepartment.getText()).to.eventually.equal(("SOFTWARE, SYSTEM, AND TEST ENGINEERING"));
+            });
 
-    //             await browser.wait(ec.visibilityOf(element(by.css(".search-result ul li:last-child")), 5000));
-    //         });
+            it("should have job with proper location", async () => {
+                const searchedJob = careerPage.jobResult(careerPage.formatJobTitle(".NET Developer (Middle/Senior/Lead)"));
+                const locationOfJob = careerPage.checkLocationOfJob(searchedJob);
+                expect(locationOfJob.getText()).to.eventually.equal("DEBRECEN, HUNGARY OR REMOTE");
+            });
 
-    //         it("should find the proper job", async () => {
-    //             const jobResult = element(by.css(".search-result__item .search-result__item-info a[href*='net-developer-middle-senior-lead_debrecen_hungary']"));
-    //             expect(jobResult.getText()).to.eventually.equal(".NET Developer (Middle/Senior/Lead)");
-    //         });
+            it("should have an apply button for job", async () => {
+                const applyButton = await careerPage.findJobsApplyButton(".NET Developer (Middle/Senior/Lead)");
+                expect(applyButton.isDisplayed()).to.eventually.be.true;
+                expect(applyButton.getText()).to.eventually.equal("VIEW AND APPLY");
+                await applyButton.click();
+            });
 
-    //         it("should have job with proper department", async () => {
-    //             const selected = await element.all(by.css(".selected-items li"));
-    //             expect(selected).to.have.length(1);
-    //             expect(selected[0].getText()).to.eventually.equal(("Software, System, and Test Engineering").toUpperCase());
-    //         });
+            describe("Applying to position", async () => {
 
-    //         it("should have job with proper location", async () => {
-    //             const jobResult = element(by.css(".search-result__item .search-result__item-info a[href*='net-developer-middle-senior-lead_debrecen_hungary']"));
-    //             const locationOfJob = jobResult.element(by.xpath("./../../strong"))
-    //             expect(locationOfJob.getText()).to.eventually.equal("DEBRECEN, HUNGARY OR REMOTE");
-    //         });
+                before(async () => {
+                    await browser.wait(ec.urlContains(careerPage.formatJobTitle(".NET Developer (Middle/Senior/Lead)")), 10000);
+                });
 
-    //         it("should have an apply button for job", async () => {
-    //             const applyButton = element(by.css(".search-result__item-footer a[href*='net-developer-middle-senior-lead_debrecen_hungary']"));
-    //             expect(applyButton.isDisplayed()).to.eventually.be.true;
-    //             expect(applyButton.getText()).to.eventually.equal("VIEW AND APPLY");
-    //             await applyButton.click();
-    //         });
+                it("should have proper position name in the description", async () => {
+                    expect(careerPage.positionName.getText()).to.eventually.contain(".NET Developer (Middle/Senior/Lead)");
+                });
 
-    //         describe("Applying to position", async () => {
-
-    //             beforeEach(async () => {
-    //                 const jobResult = element(by.css(".search-result__item .search-result__item-info a[href*='net-developer-middle-senior-lead_debrecen_hungary']"));
-    //                 await jobResult.click();
-    //                 await browser.wait(ec.urlIs("https://www.epam.com/careers/job-listings/job.72856.net-developer-middle-senior-lead_debrecen_hungary"), 5000);
-    //             });
-
-    //             it("should have proper position name in the description", async () => {
-    //                 const positionName = element(by.css(".recruiting-page__header h1"));
-    //                 expect(positionName.getText()).to.eventually.contain(".NET Developer (Middle/Senior/Lead)");
-    //             });
-
-    //             it("should have proper location in the description", async () => {
-    //                 const positionLocation = element(by.css(".recruiting-page__location"));
-    //                 expect(positionLocation.getText()).to.eventually.equal("Debrecen, Hungary or Remote");
-    //             });
-    //         });
-    //     });
+                it("should have proper location in the description", async () => {
+                    expect(careerPage.positionLocation.getText()).to.eventually.equal("Debrecen, Hungary or Remote");
+                });
+            });
+        });
     });
 });
--- pageObject/CareerPage.js
+++ pageObject/CareerPage.js
@@ -13,13 +13,14 @@ class CareerPage {
         this.departmentInput = element(by.css("div.job-search__departments div.selected-params"));
         this.departmentSelectDropdown = element(by.css(".multi-select-dropdown ul"));
         this.departmentSelect = department => element(by.css(`.multi-select-dropdown ul input[data-value='${department}'] + span`));
-        this.checkSelectedDepartment = element.all(by.css(".selected-items li"));
+        this.checkSelectedDepartment = element(by.css("ul.selected-items li:first-child"));
         
         this.findButton = element(by.css("#jobSearchFilterForm > button"));
         this.searchResults = element(by.css(".search-result ul li:last-child"));
 
         this.formatJobTitle = job => job.toLowerCase().replace(/[\s\/]/g, "-").replace(/[()]/g, "");
         this.jobResult = job => element(by.css(`.search-result__item .search-result__item-info a[href*='${job}']`));
+        this.checkLocationOfJob = job => job.element(by.xpath("./../../strong"));
         this.jobsApplyButton = job => job.element(by.xpath("./../../../div[@class='search-result__item-footer search-result__clearfix']/div/div/a"));
 
         this.positionName = element(by.css(".recruiting-page__header h1"));
@@ -29,7 +30,7 @@ class CareerPage {
 
     async load() {
         await browser.get("https://www.epam.com/careers");
-        await browser.wait(ec.elementToBeClickable(this.logo), 10000);
+        return await browser.wait(ec.elementToBeClickable(this.logo), 10000);
     }
 
     async selectLocation(country, city) {
@@ -40,26 +41,28 @@ class CareerPage {
         await countryToClick.click();
 
         const cityToClick = this.citySelect(country, city);
-        await cityToClick.click()
+        return await cityToClick.click()
     }
 
     async selectDepartment(department) {
         await this.departmentInput.click();
         await browser.wait(ec.visibilityOf(this.departmentSelectDropdown), 3000);
         const departmentToClick = this.departmentSelect(department);
-        await departmentToClick.click();
+        return await departmentToClick.click();
     }
 
-    async searchForJobs() {
+    async clickFindButton() {
         await this.findButton.click();
-        await browser.wait(ec.visibilityOf(this.searchResults), 5000);
+        return await browser.wait(ec.visibilityOf(this.searchResults), 5000);
     }
 
-    async clickJobsApplyButton(job) {
+    async findJobsApplyButton(job) {
         const formattedJobTitle = this.formatJobTitle(job);
         const jobToApply = this.jobResult(formattedJobTitle);
         const applyButtonOfJob = this.jobsApplyButton(jobToApply);
-        await applyButtonOfJob.click();
+        return new Promise((resolve) => {
+            resolve(applyButtonOfJob);
+        });
     }
 
 }
