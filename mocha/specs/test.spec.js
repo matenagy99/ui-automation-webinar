4c2f117e6b1dc7c14be1bdb1df88de6e59721590
Nagy Máté <Mate_Nagy2@epam.com>
2022-10-15 10:16:07 +0200

    Using data-driven approach

--- mocha/specs/test.spec.js
+++ mocha/specs/test.spec.js
@@ -2,82 +2,85 @@
 
 const CareerPage = require("../../pageObject/CareerPage");
 const careerPage = new CareerPage();
+const testData = require("./test.json");
 
-describe("Search for job", async () => {
+testData.forEach(search => {
+    describe("Search for job", async () => {
 
-    before(async () => {
-        await careerPage.load();
-    });
-
-    describe("Careers page", async () => {
-        it("should be opnened", async () => {
-            await careerPage.acceptCookiesButton.click();
-            expect(careerPage.logo.isDisplayed()).to.eventually.be.true;
-        });
-    });
-
-    describe("Search form", async () => {
-        
-        it("should be displayed", async () => {
-            expect(careerPage.searchForm.isDisplayed()).to.eventually.be.true;
+        before(async () => {
+            await careerPage.load();
         });
 
-        describe("Location filter box", async () => {
-            it("should provide a way to filter to a specific location", async () => {
-                expect(careerPage.locationInput.isDisplayed()).to.eventually.be.true;
-                await careerPage.selectLocation("Hungary", "Debrecen");
-                expect(careerPage.checkLocation.getText()).to.eventually.equal("Debrecen");
+        describe("Careers page", async () => {
+            it("should be opnened", async () => {
+                await careerPage.acceptCookiesButton.click();
+                expect(careerPage.logo.isDisplayed()).to.eventually.be.true;
             });
         });
 
-        describe("Department filter box", async () => {
-            it("should provide a way to filter to a specific department", async () => {
-                expect(careerPage.departmentInput.isPresent()).to.eventually.be.true;
-                await careerPage.selectDepartment("Software, System, and Test Engineering");
-                expect(careerPage.checkSelectedDepartment.getText()).to.eventually.equal(("SOFTWARE, SYSTEM, AND TEST ENGINEERING"));
-            });
-        });
-        
-        describe("Searching", async () => {
-
-            before(async () => {
-                await careerPage.clickFindButton();
+        describe("Search form", async () => {
+            
+            it("should be displayed", async () => {
+                expect(careerPage.searchForm.isDisplayed()).to.eventually.be.true;
             });
 
-            it("should find the proper job", async () => {
-                const searchedJob = careerPage.jobResult(careerPage.formatJobTitle(".NET Developer (Middle/Senior/Lead)"));
-                expect(searchedJob.getText()).to.eventually.equal(".NET Developer (Middle/Senior/Lead)");
+            describe("Location filter box", async () => {
+                it("should provide a way to filter to a specific location", async () => {
+                    expect(careerPage.locationInput.isDisplayed()).to.eventually.be.true;
+                    await careerPage.selectLocation(search.country, search.city);
+                    expect(careerPage.checkLocation.getText()).to.eventually.equal(search.city);
+                });
             });
 
-            it("should have job with proper department", async () => {
-                expect(careerPage.checkSelectedDepartment.getText()).to.eventually.equal(("SOFTWARE, SYSTEM, AND TEST ENGINEERING"));
+            describe("Department filter box", async () => {
+                it("should provide a way to filter to a specific department", async () => {
+                    expect(careerPage.departmentInput.isPresent()).to.eventually.be.true;
+                    await careerPage.selectDepartment(search.department);
+                    expect(careerPage.checkSelectedDepartment.getText()).to.eventually.equal((search.department.toUpperCase()));
+                });
             });
-
-            it("should have job with proper location", async () => {
-                const searchedJob = careerPage.jobResult(careerPage.formatJobTitle(".NET Developer (Middle/Senior/Lead)"));
-                const locationOfJob = careerPage.checkLocationOfJob(searchedJob);
-                expect(locationOfJob.getText()).to.eventually.equal("DEBRECEN, HUNGARY OR REMOTE");
-            });
-
-            it("should have an apply button for job", async () => {
-                const applyButton = await careerPage.findJobsApplyButton(".NET Developer (Middle/Senior/Lead)");
-                expect(applyButton.isDisplayed()).to.eventually.be.true;
-                expect(applyButton.getText()).to.eventually.equal("VIEW AND APPLY");
-                await applyButton.click();
-            });
-
-            describe("Applying to position", async () => {
+            
+            describe("Searching", async () => {
 
                 before(async () => {
-                    await browser.wait(ec.urlContains(careerPage.formatJobTitle(".NET Developer (Middle/Senior/Lead)")), 10000);
+                    await careerPage.clickFindButton();
                 });
 
-                it("should have proper position name in the description", async () => {
-                    expect(careerPage.positionName.getText()).to.eventually.contain(".NET Developer (Middle/Senior/Lead)");
+                it("should find the proper job", async () => {
+                    const searchedJob = careerPage.jobResult(careerPage.formatJobTitle(search.job));
+                    expect(searchedJob.getText()).to.eventually.equal(search.job);
                 });
 
-                it("should have proper location in the description", async () => {
-                    expect(careerPage.positionLocation.getText()).to.eventually.equal("Debrecen, Hungary or Remote");
+                it("should have job with proper department", async () => {
+                    expect(careerPage.checkSelectedDepartment.getText()).to.eventually.equal((search.department.toUpperCase()));
+                });
+
+                it("should have job with proper location", async () => {
+                    const searchedJob = careerPage.jobResult(careerPage.formatJobTitle(search.job));
+                    const locationOfJob = careerPage.checkLocationOfJob(searchedJob);
+                    expect(locationOfJob.getText()).to.eventually.contain(`${search.city.toUpperCase()}, ${search.coutryShortForm.toUpperCase()}`);
+                });
+
+                it("should have an apply button for job", async () => {
+                    const applyButton = await careerPage.findJobsApplyButton(search.job);
+                    expect(applyButton.isDisplayed()).to.eventually.be.true;
+                    expect(applyButton.getText()).to.eventually.equal("VIEW AND APPLY");
+                    await applyButton.click();
+                });
+
+                describe("Applying to position", async () => {
+
+                    before(async () => {
+                        await browser.wait(ec.urlContains(careerPage.formatJobTitle(search.job)), 10000);
+                    });
+
+                    it("should have proper position name in the description", async () => {
+                        expect(careerPage.positionName.getText()).to.eventually.contain(search.job);
+                    });
+
+                    it("should have proper location in the description", async () => {
+                        expect(careerPage.positionLocation.getText()).to.eventually.contain(`${search.city}, ${search.coutryShortForm}`);
+                    });
                 });
             });
         });
